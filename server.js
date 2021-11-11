c139d0d21ff88800b447a4e7ac13357f21de8f8c
gunawan <gunaduadelapan@gmail.com>
2021-11-11 23:29:25 +0800

    setting routes di server.js

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app=express();
 // parse aplication/json
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+
+// paggil routes nya
+var routes=require('./routes');
+routes(app);
+
 app.listen(3000,()=>{
     console.log('server started on port');
 });
